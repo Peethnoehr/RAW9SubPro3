e3368062fb985323bc81b83bef6fc8ccc9a02738
MarcoFGreve <44023429+MarcoFGreve@users.noreply.github.com>
2019-12-04 21:36:05 +0100

    Attempted to implement livecode8 js files
    initial js files added, attempt

---
+++ Raw9ProjectRepo-master/Raw9ProjectRepo-master/WebApplication/Controllers/NamesController.cs
@@ -0,0 +1,26 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Livecode8.Controllers
+{
+    [ApiController]
+    [Route("api/names")]
+    public class NamesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new string[]
+            {
+                "Peter",
+                "John",
+                "Sue",
+                "Ellen"
+            });
+        }
+    }
+}
---
+++ Raw9ProjectRepo-master/Raw9ProjectRepo-master/WebApplication/wwwroot/js/main.js
@@ -0,0 +1,16 @@
+﻿﻿
+require.config({
+    baseUrl: "js",
+    paths: {
+        jquery: "../lib/jquery/dist/jquery",
+        knockout: "../lib/knockout/build/output/knockout-latest.debug",
+        dataService: "services/dataService"
+
+    }
+});
+
+require(["knockout", "app"], function(ko, app, ds) {
+    //console.log(app.name);
+
+    ko.applyBindings(app);
+});
