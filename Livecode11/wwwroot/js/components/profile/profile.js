596944c41209397212e8424443d7e33592337ddb
Peethnoehr <36732469+Peethnoehr@users.noreply.github.com>
2019-12-08 20:50:26 +0100

    Profile Test2

---
+++ Livecode11/wwwroot/js/components/profile/profile.js
@@ -0,0 +1,20 @@
+﻿﻿define(["jquery", "knockout", "dataService", "jqcloud"], function($, ko, ds) {
+    var inputusername = ko.observable();
+    var inputpassword = ko.observable();
+    var inputemail = ko.observable();
+    var username = ko.observable("initialValue");
+    var login = function() {
+        ds.login(data => {
+            username(data.userName);
+        },inputusername(),inputpassword(),inputemail());
+    };
+    return function (params) {
+        return {
+            username,
+            inputusername,
+            inputpassword,
+            inputemail,
+            login
+        };
+    };
+});
