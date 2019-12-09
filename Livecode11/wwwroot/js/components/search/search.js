a11c006d859bc5ac7639302c70d6453055f26180
Peethnoehr <36732469+Peethnoehr@users.noreply.github.com>
2019-12-09 17:49:01 +0100

    Search WIP
    Search WIP

--- Livecode11/wwwroot/js/components/search/search.js
+++ Livecode11/wwwroot/js/components/search/search.js
@@ -1,17 +1,26 @@
 ﻿﻿define(["jquery", "knockout", "dataService", "jqcloud"], function($, ko, ds) {
-    
+    var searchedPost = ko.observableArray([]);
+    var postdata = ko.observable();
+    var parsed = ko.observable();
     var inputsearch = ko.observable();
 
     var searchPost = function(){
         alert("TestCall");
         ds.searchPost( data => {
             searchedPost(data);
+            postdata(JSON.stringify(data[1].title));
+            parsed(JSON.parse(data));
         },inputsearch());
     };
     return function (params) {
         return {
             inputsearch,
+            searchedPost,
+            postdata,
+            parsed,
             searchPost
         };
     };
 });
+
+// 
