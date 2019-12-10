02ec6af9341c48223987d9542ba02c171e5fa208
MarcoFGreve <44023429+MarcoFGreve@users.noreply.github.com>
2019-12-10 12:33:15 +0100

    Added navbar-scoped observable for sharing username between components

--- Livecode11/wwwroot/js/components/login/login.js
+++ Livecode11/wwwroot/js/components/login/login.js
@@ -1,13 +1,14 @@
 ﻿﻿define(["jquery", "knockout", "dataService", "jqcloud"], function($, ko, ds) {
         var inputusername = ko.observable();
         var inputpassword = ko.observable();
-        var username = ko.observable("initialValue");
+        var username; // = ko.observable("initialValue");
         var login = function() {
             ds.login(data => {
                 username(data.userName);
         },inputusername(),inputpassword());
         };
     return function (params) {    
+        username = params.userName;
         return {
             username,
             inputusername,
