ca37578ab240db9f75ae2151ae973740ab9bcd42
MarcoFGreve <44023429+MarcoFGreve@users.noreply.github.com>
2019-12-14 13:10:57 +0100

    WIP markings, added deletemark button

--- Livecode11/wwwroot/js/components/marking/marking.js
+++ Livecode11/wwwroot/js/components/marking/marking.js
@@ -3,13 +3,20 @@
     var markings = ko.observableArray([]);
     var username;// = ko.observable("Test");
     
+    var deleteMarking = function(marking){
+        alert("run marking.js.deleteMarking");
+        ds.deleteMark(data => {
+            alert(JSON.stringify(data));
+    }, username, marking.id);
+    };
     return function (params) {
         username = params.userName;
         ds.getMarkings(markings, username);
     
         return {
             markings,
-            username
+            username,
+            deleteMarking
         };
     };
 });
